27217497cf3013b20e2c2767e715e291b2d8189e
Cihan Çallı <c.calli@diten.tech>
2023-09-06 21:33:19 +0300

    Create and Inject LocalDataSource

---
+++ app/src/main/java/com/zerdasoftware/foody/data/LocalDataSource.kt
@@ -0,0 +1,18 @@
+package com.zerdasoftware.foody.data
+
+import com.zerdasoftware.foody.data.database.RecipesDao
+import com.zerdasoftware.foody.data.database.RecipesEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(
+    private val recipesDao:RecipesDao
+) {
+    fun readDatabase(): Flow<List<RecipesEntity>> {
+        return recipesDao.readRecipes()
+    }
+
+    suspend fun insertRecipes(recipesEntity: RecipesEntity) {
+        recipesDao.insertRecipes(recipesEntity)
+    }
+}
