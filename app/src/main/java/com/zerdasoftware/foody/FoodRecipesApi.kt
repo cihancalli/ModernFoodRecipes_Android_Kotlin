d3ddf5261c2e43d9642d7c28b2289ed1c7384377
Cihan Çallı <c.calli@diten.tech>
2023-09-06 12:27:39 +0300

    Create RecipesApi interface

---
+++ app/src/main/java/com/zerdasoftware/foody/FoodRecipesApi.kt
@@ -0,0 +1,12 @@
+package com.zerdasoftware.foody
+
+import com.zerdasoftware.foody.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipesApi {
+
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(@QueryMap queries:Map<String,String>):Response<FoodRecipe>
+}
