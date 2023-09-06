77c5c3f706a37173420320d0f32acdca5c00ade7
Cihan Çallı <c.calli@diten.tech>
2023-09-06 14:49:50 +0300

    Inject Remote Data Source class

--- app/src/main/java/com/zerdasoftware/foody/RemoteDataSource.kt
+++ app/src/main/java/com/zerdasoftware/foody/RemoteDataSource.kt
@@ -1,4 +1,13 @@
 package com.zerdasoftware.foody
 
-class RemoteDataSource() {
+import com.zerdasoftware.foody.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+    suspend fun getRecipes(queries:Map<String,String>): Response<FoodRecipe>{
+        return foodRecipesApi.getRecipes(queries)
+    }
 }
