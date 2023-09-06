cc1d25a1436c08582eb9e93dbb46b28b13724eb9
Cihan Çallı <c.calli@diten.tech>
2023-09-06 12:18:29 +0300

    Create Constants class

---
+++ app/src/main/java/com/zerdasoftware/foody/Constants.kt
@@ -0,0 +1,9 @@
+package com.zerdasoftware.foody
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "e8030bdc94d640649b61839b894b93fa"
+    }
+}
